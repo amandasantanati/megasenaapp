d853dd2a7b07f8c53794cfcd97bb600796202793
Amanda Santana <asantana@thoughtworks.com>
2016-12-25 12:48:40 -0300

    Adiciona a lógica do calculo e exibição dos jogos

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+import random
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,5 +9,30 @@ app = Flask(__name__)
 def index():
   return render_template('index.html') 
 
+@app.route('/calculate', methods=['POST'])
+def calculate():
+  megasena_expense = 3.00
+
+  money = float(request.form['money'])
+
+  games_quantity = int(money / megasena_expense)
+
+  games = []
+
+  for i in range(0, games_quantity):
+    a_game = set()
+
+    while True:
+      number = random.randrange(1, 61, 3)
+      a_game.add(number)
+
+      if len(a_game) == 6:
+        games.append(a_game)
+        break
+
+
+  return render_template('calculate.html', money=money, games_quantity=games_quantity, games=games)
+
 if __name__ == '__main__':
+  app.debug = True
   app.run()
